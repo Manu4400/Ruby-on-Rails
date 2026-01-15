310e9826a06ff14b0b936d5bffdec287b3dc9413
Manu4400 <manoharmr1234@gmail.com>
2026-01-15 21:42:06 +0530

    Add Day4 folder

---
+++ Day4/1.rb
@@ -0,0 +1,46 @@
+name = :ruby
+str = "Manu"
+str = str + "MR"
+=begin
+name = name + :on_rails
+This will cause an error because Ruby does not allow concatenation of a String and a Symbol using the '+' operator.
+=end
+
+puts name.class
+puts str.class
+puts name.object_id
+puts str.object_id
+
+5.times do 
+  puts str.object_id
+end
+
+class Person
+  @@count = 0
+  attr_accessor :name, :id, :present
+
+  def initialize
+    @@count += 1
+  end
+
+  def display
+    puts "Name: #{@name}, Employee ID: #{@id}, Attendance: #{@present}"
+  end
+
+  def self.count
+    puts "Total Employees: #{@@count}"
+  end
+end
+
+p1 = Person.new
+p1.name = "Manu"
+p1.id = 1
+p1.present = true
+p1.display
+
+p2 = Person.new
+p2.name = "Ravi"
+p2.id = 2
+p2.present = false
+p2.display
+
